ea2958bdb6708f29dfb1eee00ba2634a340d267e
VZamora5 <victoriazamora007@gmail.com>
2022-02-19 14:51:49 -0500

    Drive command made

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,10 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.DriveWithJoystick;
+import frc.robot.subsystems.DriveSystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,12 +20,18 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  DriveSystem drive;
+  DriveWithJoystick driveCommand;
+  Joystick joy;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+
+    joy = new Joystick(0);
+    drive = new DriveSystem();
+    driveCommand = new DriveWithJoystick(drive, joy);
+    drive.setDefaultCommand(driveCommand);
+    
     // Configure the button bindings
     configureButtonBindings();
   }
@@ -43,6 +51,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+  return null;
   }
 }
